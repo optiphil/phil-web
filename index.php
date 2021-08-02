288dbe8640a891af4ab4464a92bb2de91683896d
Philip <87368971+optiphil@users.noreply.github.com>
2021-08-02 04:33:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <section id="banner">
-        <img src="./Images/lazy logo.png">
+        <img src="./images/lazy logo.png">
         <div class="banner-text">
             <h1>Website Designer</h1>
             <p>we specify in both front end and backend</p>
@@ -73,7 +73,7 @@
               </div>
             </div>
             <div class="features-img">
-                 <img src="./Images/lazy designer.jpg">
+                 <img src="./images/lazy designer.jpg">
             </div>
         </div>
     </section>
@@ -103,7 +103,7 @@
     <div class="testimonial-row">
         <div class="testimonial-col">
             <div class="user">
-                <img src="./Images/facebook-img1.jpg" alt="">
+                <img src="./images/facebook-img1.jpg" alt="">
                 <div class="user-info">
                     <h4>Praise Philips <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z"/></svg></h4>
                     <small><a href="https://www.facebook.com/praise.philips.547">Praise Philips</a></small>
@@ -113,7 +113,7 @@
         </div>
         <div class="testimonial-col">
             <div class="user">
-                <img src="./Images/facebook-img2.jpg " alt="">
+                <img src="./images/facebook-img2.jpg " alt="">
                 <div class="user-info">
                     <h4>Daniel Aderinola <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z"/></svg></h4>
                     <small><a href="https://www.facebook.com/daniel.jordan.9634340">Daniel Jordan</a></small>
@@ -124,7 +124,7 @@
         </div>
         <div class="testimonial-col">
             <div class="user">
-                <img src="./Images/facebook-img3.jpg " alt="">
+                <img src="./images/facebook-img3.jpg " alt="">
                 <div class="user-info">
                     <h4>Victor Odili <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z"/></svg></h4>
                     <small><a href="https://www.facebook.com/victorjr.marves">Victor jr marves</a></small>
